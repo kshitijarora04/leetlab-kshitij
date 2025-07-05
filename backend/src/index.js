0d7d5eaa7099559a76a88f4e9fd9ded74a908825
kshitijarora04 <79055679+kshitijarora04@users.noreply.github.com>
2025-07-05 22:54:16 +0530

    origin cors fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_CLIENT_URL,
     credentials: true,
   })
 );
