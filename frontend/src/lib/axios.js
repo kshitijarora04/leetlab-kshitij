1db45f27255b9c8a926d74fb47da12c442f595ab
kshitijarora04 <79055679+kshitijarora04@users.noreply.github.com>
2025-07-06 00:33:03 +0530

    deployment fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,12 @@
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:8000/api/v1"
-      : "https://leetlab-kshitij-8t9i.vercel.app/api/v1",
+      : process.env.BACKEND_VERCEL_URL,
   withCredentials: true,
 });
