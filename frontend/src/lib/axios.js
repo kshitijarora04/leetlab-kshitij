f282bd4b6cc89acfa7731a9d94199e96e929fdde
kshitijarora04 <79055679+kshitijarora04@users.noreply.github.com>
2025-07-06 00:00:37 +0530

    axios fix for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:8000/api/v1"
-      : "/api/v1",
+      : "https://leetlab-kshitij-8t9i.vercel.app/api/v1",
   withCredentials: true,
 });
-
